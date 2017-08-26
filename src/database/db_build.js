7009896c2e14949d2f11c90e0a7c0298a8c35d58
Vered Rekanati <veredrec@gmail.com>
2017-08-26 07:53:39 +0300

    add content to build.js and connection.js relates #7

---
+++ src/database/db_build.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const dbconnection = require('./db_connection');
+
+const buildScript = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
+
+dbconnection.query(buildScript, (err, res) => {
+  if (err) {
+    console.log("Error with database ", err);
+  } else {
+    console.log('Volunteer table succesfully created :', res)  
+  }
+})
