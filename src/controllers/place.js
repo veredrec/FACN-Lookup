a759414069913b3e507fe36b555a2156917ac95c
Vered Rekanati <veredrec@gmail.com>
2017-09-02 07:33:07 +0300

    add helper for uppercase relates #36

--- src/controllers/place.js
+++ src/controllers/place.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const places = require('./../database/db_build.js');
 
-exports.get = (req, res, next) => {
-  const { place } = req.params;
+exports.get = (request, response, next) => {
+  console.log("params ", request.params);
+  const { place } = request.params;
 
 if (places.includes(place)) {
-  return res.render('place', { place });
+  return response.render('place', { place });
 }
   next();
 };
+
+//This part doesn't work
--- src/controllers/router.js
+++ src/controllers/router.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const getOptions = require('./../queries/getOptions.js');
 const router = express.Router();
-
 const home = require('./home');
-// const place = require('./../queries/getPlace');
+const place = require('./place')
+const getPlace = require('./../queries/getPlace');
 const error = require('./error');
 
 router.get('/', (request, response) => {
@@ -20,7 +20,19 @@ router.get('/', (request, response) => {
   })
 });
 
-// router.get('/places/:place', places.get);
+router.get('/places/:place', (request, response) => {
+  console.log("stating getPlace from router");
+  getPlace((err, place) => {
+    if (err) {
+      console.log("err", err);
+      response.send('<h1>Sorry, there is a problem with our data</h1>');
+    } else {
+      console.log("takes data from db in place");
+      return response.render('place', { place });
+    }
+  })
+});
+
 router.use(error.client);
 router.use(error.server);
 
